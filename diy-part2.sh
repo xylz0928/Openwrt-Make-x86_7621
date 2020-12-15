7291d80d89917723d109ef802bde2bf421937ea5
然后七年 <34766756+xylz0928@users.noreply.github.com>
2020-12-15 09:05:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,26 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
+
+# Add ServerChan
+# git clone https://github.com/tty228/luci-app-serverchan.git feeds/luci/applications/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+
+# Add Onliner
+# git clone https://github.com/rufengsuixing/luci-app-onliner.git feeds/luci/applications/luci-app-onliner
+git clone https://github.com/rufengsuixing/luci-app-onliner.git package/luci-app-onliner
+
+# Add OpenAppFilter
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+
+# Add ServerChan-DINGDING
+git clone https://github.com/zzsj0928/luci-app-serverchand package/luci-app-serverchand
+
+
+# Fix Multi-use on one physical port 
+# 修复 5.4.68-5.4.69内核 于 MIPS 架构 单线复用BUG
+# 取自 @AmadeusGhost， 原更新内容为 generic: limit commit "ramips/mediatek: improve GRO performance, fix PPE packet parsing" to mediatek target only
+# wget https://github.com/AmadeusGhost/lede/commit/7a49d2cf99bd59506bbd9239e0bde81a61f93c40.patch
+# git apply 7a49d2cf99bd59506bbd9239e0bde81a61f93c40.patch
