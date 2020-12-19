3675d7c8b1362ad2f82a2f8be3fb95262df11513
然后七年 <34766756+xylz0928@users.noreply.github.com>
2020-12-19 09:58:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,11 +29,6 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # Add ServerChan-DINGDING
 git clone https://github.com/zzsj0928/luci-app-serverchand package/luci-app-serverchand
 
-# Add ADGuardHome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-
-# Add KoolProxyR Plus+
-git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 
 # Fix Multi-use on one physical port 
 # 修复 5.4.68-5.4.69内核 于 MIPS 架构 单线复用BUG
