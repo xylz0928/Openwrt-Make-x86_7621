13e131e8e9794c2c786d7651af08b706fb997bb2
然后七年 <34766756+xylz0928@users.noreply.github.com>
2021-05-06 13:10:25 +0800

    Update diy-x86-opofficial-part2.sh

--- diy-x86-opofficial-part2.sh
+++ diy-x86-opofficial-part2.sh
@@ -14,6 +14,10 @@
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean ./package/lean/
 chmod -R 755 ./package/lean/*
 
+# autosamba 依赖 samba
+svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-samba ./package/collected/luci-app-samba
+chmod -R 755 ./package/collected/luci-app-samba
+
 # 修改版本号-webui
 modelmark=R`TZ=UTC-8 date +%Y-%m-%d -d +"0"days`' by xylz0928'
 sed -i "s/DISTRIB_REVISION='R[0-9]*\.[0-9]*\.[0-9]*/DISTRIB_REVISION='$modelmark/g" ./package/lean/default-settings/files/zzz-default-settings
