d6a2b2cbc447c3d9c2f437608a492e1a9424c8a0
然后七年 <34766756+xylz0928@users.noreply.github.com>
2021-05-28 22:52:53 +0800

    Update diy-x86-part2.sh

--- diy-x86-part2.sh
+++ diy-x86-part2.sh
@@ -115,20 +115,20 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-v
 
 # Add Passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-rm -rf ./package/lean/dns2socks
-rm -rf ./package/lean/ipt2socks
-rm -rf ./package/lean/kcptun
-rm -rf ./package/lean/microsocks
-rm -rf ./package/lean/shadowsocksr-libev
-rm -rf ./package/lean/simple-obfs
-rm -rf ./package/lean/tcping
-rm -rf ./package/lean/trojan
+# rm -rf ./package/lean/dns2socks
+# rm -rf ./package/lean/ipt2socks
+# rm -rf ./package/lean/kcptun
+# rm -rf ./package/lean/microsocks
+# rm -rf ./package/lean/shadowsocksr-libev
+# rm -rf ./package/lean/simple-obfs
+# rm -rf ./package/lean/tcping
+# rm -rf ./package/lean/trojan
 # rm -rf ./package/lean/v2ray
-rm -rf ./package/lean/v2ray-plugin
-rm -rf ./package/openwrt-passwall/naiveproxy
-rm -rf ./package/openwrt-passwall/tcping
-rm -rf ./package/openwrt-passwall/xray-core
-rm -rf ./package/openwrt-passwall/v2ray
+# rm -rf ./package/lean/v2ray-plugin
+# rm -rf ./package/openwrt-passwall/naiveproxy
+# rm -rf ./package/openwrt-passwall/tcping
+# rm -rf ./package/openwrt-passwall/xray-core
+# rm -rf ./package/openwrt-passwall/v2ray
 # rm -rf ./package/openwrt-passwall/v2ray-plugin
 # rm -rf ./feeds/helloworld/naiveproxy
 # rm -rf ./feeds/helloworld/tcping
