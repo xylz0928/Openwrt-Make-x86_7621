61af86de50ae9647424fe2bd9fe5b865a67b446d
然后七年 <34766756+xylz0928@users.noreply.github.com>
2021-08-01 07:39:39 +0800

    cancel 5.10 for MIPS

--- diy-x86-part2.sh
+++ diy-x86-part2.sh
@@ -11,8 +11,8 @@
 #
 
 
-#切换ramips内核为 5.10
-sed -i 's/5.4/5.10/g' ./target/linux/ramips/Makefile
+# 切换ramips内核为 5.10
+# sed -i 's/5.4/5.10/g' ./target/linux/ramips/Makefile
 
 # 修复核心及添加温度显示
 # sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
