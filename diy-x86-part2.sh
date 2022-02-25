ab8fcc5c3ad0369c8b467c7071057d3e3d473dc3
然后七年 <34766756+xylz0928@users.noreply.github.com>
2022-02-26 00:57:28 +0800

    upstream switched to 5.15 for x86

--- diy-x86-part2.sh
+++ diy-x86-part2.sh
@@ -15,7 +15,7 @@
 sed -i 's/5.4/5.10/g' ./target/linux/ramips/Makefile
 
 # 切换x86内核为 5.10
-sed -i 's/5.4/5.10/g' ./target/linux/x86/Makefile
+# sed -i 's/5.4/5.10/g' ./target/linux/x86/Makefile
 
 # 修复核心及添加温度显示
 # sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
