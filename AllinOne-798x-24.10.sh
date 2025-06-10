0cff73a0b4f79fbcb700199820ece5d06a305d81
然后七年 <34766756+xylz0928@users.noreply.github.com>
2025-06-10 14:42:33 +0800

    Create AllinOne-798x-24.10.sh

---
+++ AllinOne-798x-24.10.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+cd ~
+git clone https://github.com/padavanonly/immortalwrt-mt798x-24.10
+cd immortalwrt-mt798x-24.10
+wget https://github.com/xylz0928/Openwrt-Make-x86_7621/raw/main/diy-padavanonly-part1.sh && \
+wget https://github.com/xylz0928/Openwrt-Make-x86_7621/raw/main/diy-hanwckf-part2.sh && \
+wget https://github.com/xylz0928/Openwrt-Make-x86_7621/raw/main/MakeMenu.360T7_108M.24.10.config
+bash diy-padavanonly-part1.sh && \
+./scripts/feeds update -a && ./scripts/feeds install -a && \
+bash diy-hanwckf-part2.sh
+
+cp MakeMenu.360T7_108M.24.10.config .config
+
+make download && \
+make -j16 V=s
