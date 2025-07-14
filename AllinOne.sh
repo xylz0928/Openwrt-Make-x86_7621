a49eb768a8b0b13f142aa8053c35e9517d9fc53e
然后七年 <34766756+xylz0928@users.noreply.github.com>
2025-07-14 22:26:31 +0800

    Update AllinOne.sh

--- AllinOne.sh
+++ AllinOne.sh
@@ -3,9 +3,9 @@ set -e
 cd ~
 git clone https://github.com/coolsnowwolf/lede
 cd lede
-wget https://github.com/xylz0928/Openwrt-Make-x86_7621/raw/main/diy-x86-part1.sh && \
-wget https://github.com/xylz0928/Openwrt-Make-x86_7621/raw/main/diy-x86-part2.sh && \
-wget https://github.com/xylz0928/Openwrt-Make-x86_7621/raw/main/MakeMenu.x86.config
+wget https://github.com/xylz0928/Openwrt-Make/raw/main/diy-x86-part1.sh && \
+wget https://github.com/xylz0928/Openwrt-Make/raw/main/diy-x86-part2.sh && \
+wget https://github.com/xylz0928/Openwrt-Make/raw/main/MakeMenu.x86.config
 bash diy-x86-part1.sh && \
 ./scripts/feeds update -a && ./scripts/feeds install -a && \
 bash diy-x86-part2.sh
