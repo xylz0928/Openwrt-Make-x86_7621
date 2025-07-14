b7c35ae6a5e6f7f46e64bea1e8a71867c14be0fa
然后七年 <34766756+xylz0928@users.noreply.github.com>
2025-07-14 22:23:08 +0800

    Update AllinOne-798x-24.10-6.6.sh

--- AllinOne-798x-24.10-6.6.sh
+++ AllinOne-798x-24.10-6.6.sh
@@ -3,9 +3,9 @@ set -e
 cd ~
 git clone -b openwrt-24.10-6.6 --single-branch --filter=blob:none https://github.com/padavanonly/immortalwrt-mt798x-6.6 immortalwrt-mt798x-24.10-6.6
 cd immortalwrt-mt798x-24.10-6.6
-wget https://github.com/xylz0928/Openwrt-Make-x86_7621/raw/main/diy-x86-part1.sh && \
-wget https://github.com/xylz0928/Openwrt-Make-x86_7621/raw/main/diy-24.10-part2.sh && \
-wget https://github.com/xylz0928/Openwrt-Make-x86_7621/raw/main/MakeMenu.mt7981.24.10.6.6.config
+wget https://github.com/xylz0928/Openwrt-Make/raw/main/diy-x86-part1.sh && \
+wget https://github.com/xylz0928/Openwrt-Make/raw/main/diy-24.10-part2.sh && \
+wget https://github.com/xylz0928/Openwrt-Make/raw/main/MakeMenu.mt7981.24.10.6.6.config
 bash diy-x86-part1.sh && \
 ./scripts/feeds update -a && ./scripts/feeds install -a && \
 bash diy-24.10-part2.sh
